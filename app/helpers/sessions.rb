cda3b51d84c778ab516bd3e861c41de7d32bfb07
lilpaw <brandimford@gmail.com>
2015-04-23 14:32:29 -0700

    update css to show buttons, comment out sessions helper, fix login href

--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,10 +1,10 @@
-helpers do
+# helpers do
 
-  def current_user
-    if session[:access_token]
-      User.find(session[:access_token])
-    end
+#   def current_user
+#     if session[:access_token]
+#       User.find(session[:access_token])
+#     end
 
-  end
+#   end
 
-end
+# end
